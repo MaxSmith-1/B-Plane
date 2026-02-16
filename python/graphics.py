Corrected target body accel, added B-Plane calculation. MRO briefly exists within Mars S.O.I without burning, may try to integrate alternative mission that completes a hyperbolic trajectory around mars. Monte Carlo tomorrow

python/graphics.py
@@ -42,11 +42,13 @@ colors = plt.cm.tab10(np.linspace(0, 1, len(csv_files)))
 
 for i, csv_file in enumerate(csv_files):
     df = pd.read_csv(csv_file)
+
+    print(df.columns)
     spacecraft_data.append({
         'name': os.path.basename(csv_file).replace('.csv', ''),
-        'x': df['ECI_X'].values,
-        'y': df['ECI_Y'].values,
-        'z': df['ECI_Z'].values,
+        'x': df['ICRF_X'].values,
+        'y': df['ICRF_Y'].values,
+        'z': df['ICRF_Z'].values,
         'time': df['time'].values,
         'color': colors[i]
     })

python/plots.py
@@ -2,71 +2,73 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from pathlib import Path
+import math
 
 
 output_directory = Path("output")
-names = []
+names = ["MRO", "MRO"]
 
 for i, file in enumerate(output_directory.iterdir()):
 
     print(type(file))
     
-    names.append(str(file).split("\\")[1].split("_output.csv")[0])
+    try:
+        # names.append(str(file).split("\\")[1].split("_output.csv")[0])
+        sim_df = pd.read_csv(file)
+    except(PermissionError):
+        continue 
 
-    sim_df = pd.read_csv(file)
+    
 
     print(sim_df)
 
-    # Earth sphere
-    r_earth = 695700 
+    # Sun sphere
+    r_sun = 695700 
     points = np.linspace(0, 2*np.pi, 1000)
-    x_earth = r_earth*np.cos(points)
-    y_earth = r_earth*np.sin(points)
+    x_sun = r_sun*np.cos(points)
+    y_sun = r_sun*np.sin(points)
 
     plt.style.use('dark_background')
 
-    if i == 0:
-        names.insert(0, "Earth")
-
-    # ECI X-Y
+    # ICRF X-Y
     plt.figure(1)
-    plt.title("ECI X vs. Y plane")
+    plt.title("ICRF X vs. Y plane")
 
     if i == 0:
-        plt.plot(x_earth, y_earth, color='blue')
+        plt.plot(x_sun, y_sun, color='yellow')
     
     plt.plot(sim_df.ICRF_X, sim_df.ICRF_Y)
     plt.xlabel("X [km]")
     plt.ylabel("Y [km]")
     plt.axis('square')
     plt.legend("Earth")
-    plt.legend(names)
+    plt.legend(["Sun"] + names)
 
-    # ECI X-Z
+    # ICRF X-Z
     plt.figure(2)
-    plt.title("ECI X vs. Z plane")
+    plt.title("ICRF X vs. Z plane")
 
     if i == 0:
-        plt.plot(x_earth, y_earth, color='blue')
+        plt.plot(x_sun, y_sun, color='yellow')
     
     plt.plot(sim_df.ICRF_X, sim_df.ICRF_Z)
     plt.xlabel("X [km]")
     plt.ylabel("Z [km]")
     plt.axis('square')
-    plt.legend(names)
+    plt.legend(["Sun"] + names)
 
-    # ECI y-Z
+    # ICRF y-Z
     plt.figure(3)
-    plt.title("ECI Y vs. Z plane")
+    plt.title("ICRF Y vs. Z plane")
 
     if i == 0:
-        plt.plot(x_earth, y_earth, color='blue')
+        plt.plot(x_sun, y_sun, color='yellow')
     
     plt.plot(sim_df.ICRF_Y, sim_df.ICRF_Z)
     plt.xlabel("y [km]")
     plt.ylabel("Z [km]")
     plt.axis('square')
-    plt.legend(names)
+    plt.legend(["Sun"] + names)
 
     print(names)
     
@@ -116,6 +118,88 @@ for i, file in enumerate(output_directory.iterdir()):
     plt.xlabel("time [s]")
     plt.ylabel("f [rad]")
     plt.legend(names)
+
+    plt.figure(10)
+
+    sim_df["v_mag"] = np.sqrt(sim_df["Vx"] ** 2 + sim_df["Vy"] ** 2 + sim_df["Vz"] ** 2)
+    plt.title("Velocity magnitude vs. time")
+    plt.plot(sim_df.time, sim_df.v_mag)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [km/s]")
+    plt.legend(names)
+
+    plt.figure(11)
+    plt.title("Vx vs. time")
+    plt.plot(sim_df.time, sim_df.Vx)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [km/s]")
+    plt.legend(names)
+
+    plt.figure(12)
+    plt.title("Vy vs. time")
+    plt.plot(sim_df.time, sim_df.Vy)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [km/s]")
+    plt.legend(names)
+
+    plt.figure(13)
+    plt.title("Vz vs. time")
+    plt.plot(sim_df.time, sim_df.v_mag)
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [km/s]")
+    plt.legend(names)
+
+
+
+
+    # Velocity Magnitude vs. time
+
+
+ref_df = pd.read_csv("python/ref_traj.csv")
+
+target_df = pd.read_csv("output/planets/499.csv")
+print(target_df.index)
+target_df.columns = ["empty", "date", "X", "Y", "Z", "Vx", "Vy", "Vz", "emprt"]
+
+
+print(target_df)
+
+plt.figure(1)
+plt.plot(ref_df.X, ref_df.Y)
+plt.scatter(target_df.X, target_df.Y, c='red')
+plt.legend(["Sun"] + names + ["reference trajectory", "Mars"])
+ref_df["v_mag"] = np.sqrt(ref_df["VX"] ** 2 + ref_df["VY"] ** 2 + ref_df["VZ"] ** 2)
+
+plt.figure(2)
+plt.plot(ref_df.X, ref_df.Z)
+plt.scatter(target_df.X, target_df.Z, c='red')
+plt.legend(["Sun"] + names + ["reference trajectory", "Mars"])
+
+plt.figure(3)
+plt.plot(ref_df.Y, ref_df.Z)
+plt.scatter(target_df.Y, target_df.Z, c='red')
+plt.legend(["Sun"] + names + ["reference trajectory", "Mars"])
+
+
+
+
+plt.figure(10)
+plt.plot(ref_df.time, ref_df.v_mag)
+plt.legend(names + ["reference trajectory"])
+
+plt.figure(11)
+plt.plot(ref_df.time, ref_df.VX)
+plt.legend(names + ["reference trajectory"])
+
+plt.figure(12)
+plt.plot(ref_df.time, ref_df.VY)
+plt.legend(names + ["reference trajectory"])
+
+
+plt.figure(13)
+plt.plot(ref_df.time, ref_df.VZ)
+plt.legend(names + ["reference trajectory"])
+
 
 
 plt.show()